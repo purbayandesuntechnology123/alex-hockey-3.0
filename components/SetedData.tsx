482ca17939ea3e1763774d7d72c1deca64ccb842
Roshan <roshan.desuntech@gmail.com>
2025-03-05 14:13:05 +0530

    [Add]: "Home tshirt image changes"

--- components/SetedData.tsx
+++ components/SetedData.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RootState } from "@/redux/store";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { themeColor } from "@/constants/colors";
 import { useSelector } from "react-redux";
 import { RNText } from "./Text/RNText";
@@ -14,7 +14,7 @@ const SetedData = () => {
 
   console.log("-=-=-=-=-=-=-=-=---=-=->", tshirtData);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       {/* {tshirtData.map((item, index) => {
         return (
           <View style={{ borderWidth: 1, borderRadius: 8, padding: 10 }}>
@@ -39,9 +39,27 @@ const SetedData = () => {
             {selectedItem?.tshirtFrontOption?.chestStripingName}
           </RNText>
         ) : null}
+        {/* {
+          selectedItem?.tshirtFrontOption?.frontChestImage ?
+          <Image source={{uri: selectedItem?.tshirtFrontOption?.frontChestImage}} style={styles.imageStyle} />
+          : null
+        } */}
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+  imageStyle: {
+    height: 80,
+    width: 80,
+    resizeMode: "contain"
+  }
+})
 
 export default SetedData;
